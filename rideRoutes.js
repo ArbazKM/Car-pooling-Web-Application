cd962e398782f41b46cf3fe6563591a57ba3b68d
ArbazKM <arbazmokashi525@gmail.com>
2024-07-01 16:42:10 +0530

    Delete rideRoutes.js

--- rideRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { create, list } = require('../controllers/rideController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.post('/', authMiddleware, create);
-router.get('/', list);
-
-module.exports = router;
