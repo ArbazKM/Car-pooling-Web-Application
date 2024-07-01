e1d322a4959371d2d226167a09347573818ecc6b
ArbazKM <arbazmokashi525@gmail.com>
2024-07-01 16:41:09 +0530

    Delete feedbackRoutes.js

--- feedbackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { submitFeedback, getFeedback, getFeedbackCount } = require('../controllers/feedbackController');
-const router = express.Router();
-
-router.post('/', submitFeedback);
-router.get('/', getFeedback);
-router.get('/count', getFeedbackCount);
-
-module.exports = router;
