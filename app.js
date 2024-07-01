41bbbeb323c442e0a3a16da12123bdd50f80afa4
ArbazKM <arbazmokashi525@gmail.com>
2024-07-01 16:28:02 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const rideRoutes = require('./routes/rideRoutes');
+const feedbackRoutes = require('./routes/feedbackRoutes'); // Import feedback routes
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/rides', rideRoutes);
+app.use('/api/feedback', feedbackRoutes); // Use feedback routes
 
 module.exports = app;
