6b0729e22afe737d852a2849697f705c44b2f5d3
ArbazKM <arbazmokashi525@gmail.com>
2024-07-01 16:40:41 +0530

    Delete bookingRoutes.js

--- bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createBooking, listBookings } = require('../controllers/bookingController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.post('/', authMiddleware, createBooking);
-router.get('/', authMiddleware, listBookings);
-
-module.exports = router;
